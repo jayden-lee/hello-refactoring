7fb2d32cbf03275f4a71bf893bf2b50347b777d1
Jayden <gwanggi.lee@gmail.com>
2019-04-28 13:21:08 +0900

    중간 커밋

--- src/main/java/ch01/Customer.java
+++ src/main/java/ch01/Customer.java
@@ -25,13 +25,8 @@ public class Customer {
         StringBuilder statementBuilder = new StringBuilder(String.format("%s 고객님의 대여 기록 \n", getName()));
 
         for (Rental rental : rentals) {
-            double thisAmount;
-
-            // 비디오 종류별 대여료 계산
-            thisAmount = rental.getMovie().getPriceCode().calculate(rental);
-
-            // 적립 포인트를 1 포인트 증가
-            frequentRenterPoints++;
+            // 경우에 따른 적립 포인트 지급 함수를 호출
+            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
             if (rental.getMovie().getPriceCode() == Movie.PriceCode.NEW_RELEASE
@@ -40,9 +35,9 @@ public class Customer {
             }
 
             // 대여하는 비디오 정보와 대여료 출력
-            statementBuilder.append(String.format("\t%s\t%s\n", rental.getMovie().getTitle(), String.valueOf(thisAmount)));
+            statementBuilder.append(String.format("\t%s\t%s\n", rental.getMovie().getTitle(), String.valueOf(rental.getCharge())));
 
-            totalAmount += thisAmount;
+            totalAmount += rental.getCharge();
         }
         
         statementBuilder.append(String.format("누적 대여로: %s\n", String.valueOf(totalAmount)));
--- src/main/java/ch01/Rental.java
+++ src/main/java/ch01/Rental.java
@@ -13,6 +13,22 @@ public class Rental {
         this.daysRented = daysRented;
     }
 
+    /**
+     * 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+     */
+    int getFrequentRenterPoints() {
+        if (getMovie().getPriceCode() == Movie.PriceCode.NEW_RELEASE
+                && getDaysRented() > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+
+    public double getCharge() {
+        return getMovie().getPriceCode().calculate(this);
+    }
+
     public Movie getMovie() {
         return movie;
     }
